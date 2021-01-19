cbd396fc9b8dc8bac5fe7cd71a789860c11329aa
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-01-19 19:26:49 +0000

    Fix funny formatting of interviews in genios

--- background/background.js
+++ background/background.js
@@ -52,6 +52,11 @@ const converters = {
   preToParagraph: function (htmlArr) {
     return htmlArr.map(function(html) {
       html = html.replace(/<pre[^>]+>/, '').replace(/<\/pre>/, '')
+      html = html.replace(/<em class="hlt1">/g, '').replace(/<\/em>/g, '')
+      // Funny use of /// and // . for interviews for handelsblatt.com
+      html = html.replace(/\/\/\//g, '\n\n<em>')
+      html = html.replace(/\s+\/\/\s+\./g, '</em>\n\n')
+
       let parts = html.split('\n\n')
       return `<p>${parts.join('</p><p>')}</p>`
     })
