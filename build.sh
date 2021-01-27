832fcaf1c2c1238bf014901a7077d0483481905c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-01-27 09:55:12 +0000

    Add jekyll-build updates.json

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 mkdir -p build
 rm -f build/voebbot.zip
-zip -r -FS build/voebbot.zip * --exclude '*.git*' --exclude '*/.DS_Store' --exclude 'build/*' --exclude '*.sh'  --exclude '.github/*' --exclude 'screenshots/*'
+zip -r -FS build/voebbot.zip * --exclude '*.git*' \
+  --exclude '*/.DS_Store' \
+  --exclude 'build/*' \
+  --exclude '*.sh' \
+  --exclude '.github/*' \
+  --exclude 'screenshots/*' \
+  --exclude '_site/*'
