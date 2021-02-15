1038fd2b0fcbed3ffec058e55e3cd40b34801bf5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-02-15 19:16:56 +0000

    Prepare clean voebbot build dir for signing

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -ex
+
 mkdir -p build
 rm -f build/voebbot.zip
 zip -r -FS build/voebbot.zip * --exclude '*.git*' \
@@ -7,4 +9,10 @@ zip -r -FS build/voebbot.zip * --exclude '*.git*' \
   --exclude '*.sh' \
   --exclude '.github/*' \
   --exclude 'screenshots/*' \
-  --exclude '_site/*'
+  --exclude '_site/*' \
+  --exclude 'updates.json' \
+  --exclude 'index.html' \
+  --exclude 'web-ext-artifacts/*'
+
+rm -rf build/voebbot
+unzip build/voebbot.zip -d build/voebbot
