2b0f6586be1f7d3424e9b61f708b07b94fbaf61f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-03-13 20:02:59 +0000

    Add proper options page with onboarding

--- options/options.js
+++ options/options.js
@@ -1,21 +1,61 @@
-function restore() {
-    browser.storage.sync.get({username: '', password: ''}).then(function(items) {
-        var username = items.username;
-        document.getElementById("username").value = username
-        var password = items.password;
-        document.getElementById("password").value = password
-    })
+var defaults = {
+    installDate: null,
+    username: '', password: '',
+    keepStats: true,
+    stats: {}
 }
 
-function save(e) {
-    e.preventDefault()
+function restore() {
+    browser.storage.sync.get(defaults).then(function(items) {
+        document.getElementById("username").value = items.username
+        document.getElementById("password").value = items.password
+        document.getElementById("keepStats").checked = items.keepStats
+
+        if (items.installDate === null) {
+            // first run
+            browser.storage.sync.set({
+                installDate: new Date().getTime()
+            })
+            document.querySelector('#setup').setAttribute('open', true)
+        }
+    })
+    fetch('/manifest.json').then(response => response.json())
+    .then(data => {
+        var domains = data.content_scripts[0].matches.map(url => url.replace('https://', '').replace('/*', ''))
+        var ul = document.getElementById('newssites')
+        domains.forEach(domain => {
+            var li = document.createElement('li')
+            li.innerText = domain
+            ul.appendChild(li)
+        });
+    });
+}
+
+function save() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-    browser.storage.sync.set({
+    var keepStats = document.getElementById("keepStats").checked
+
+    const values = {
         username: username,
         password: password,
-    })
+        keepStats: keepStats
+    }
+    if (!keepStats) {
+        values.stats = {}
+    }
+
+    browser.storage.sync.set(values)
 }
 
+var allInputs = document.querySelectorAll('input')
+for (var i = 0; i < allInputs.length; i += 1) {
+    allInputs[i].addEventListener('input', save)
+    allInputs[i].addEventListener('change', save)
+}
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    save()
+})
+
 document.addEventListener("DOMContentLoaded", restore);
-document.querySelector("form").addEventListener("submit", save)
