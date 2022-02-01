a157e26072d6d3ff6124595e930d2daf4d4a187e
Philipp Kapfer <philipp.kapfer@gmx.at>
2022-02-01 14:30:20 +0100

    Allow replacing params without calling the converter on them

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,16 @@ const ident = (x) => x
 
 function interpolate (str, params, prefix = '', converter = ident, fallback = '') {
   if (prefix.length > 0) {
-    prefix = `${prefix}.`
+    prefix = `${prefix}\\.`
   }
   for (const key in params) {
     str = str.replace(
       new RegExp(`{${prefix}${key}}`),
       converter(params[key] || fallback)
+    )
+    str = str.replace(
+      new RegExp(`{${prefix}${key}\\.raw}`),
+      params[key] || fallback
     )
   }
   return str
