31049863daffce36286934f1b6a35bf1121cdd29
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-02-06 20:17:43 +0000

    Fix eslint style

--- src/background.js
+++ src/background.js
@@ -4,15 +4,13 @@ import update from './update.js'
 function openOptionsPage (details) {
   if (details.reason === 'install') {
     browser.runtime.openOptionsPage()
-  }
-  else if (details.reason === 'update' && details.previousVersion) {
-    console.log("Extension was updated, running update scripts...")
+  } else if (details.reason === 'update' && details.previousVersion) {
+    console.log('Extension was updated, running update scripts...')
     update.run_update(
       browser.runtime.getManifest().version,
       details.previousVersion
     )
   }
-
 }
 
 browser.runtime.onInstalled.addListener(openOptionsPage)
--- src/stats.js
+++ src/stats.js
@@ -1,7 +1,7 @@
 function increaseStats (domain) {
-  const isPrivate = browser.extension.inIncognitoContext;
+  const isPrivate = browser.extension.inIncognitoContext
   if (isPrivate) { return }
-  browser.storage.sync.get({stats: {}}).then(function(items) {
+  browser.storage.sync.get({ stats: {} }).then(function (items) {
     items.stats[domain] = (items.stats[domain] || 0) + 1
     browser.storage.sync.set({
       stats: items.stats
