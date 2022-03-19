d760ddfa02d3da67f5a07a325ca9a6a9ca418dab
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-03-19 09:26:27 +0100

    Switch from exclude to include list in dist script

--- dist.sh
+++ dist.sh
@@ -4,22 +4,7 @@ set -ex
 mkdir -p dist
 rm -f dist/bibbot.zip
 npm run build
-zip -r -FS dist/bibbot.zip * --exclude '*.git*' \
-  --exclude '*/.DS_Store' \
-  --exclude '.*' \
-  --exclude 'node_modules/*' \
-  --exclude 'dist/*' \
-  --exclude 'src/*' \
-  --exclude 'test_build/*' \
-  --exclude 'tests/*' \
-  --exclude '*.sh' \
-  --exclude '*.config.js' \
-  --exclude 'package*.json' \
-  --exclude 'screenshots/*' \
-  --exclude '_site/*' \
-  --exclude 'updates.json' \
-  --exclude 'index.html' \
-  --exclude 'web-ext-artifacts/*'
+zip -r -FS dist/bibbot.zip LICENSE README.md assets/* build/* icons/* manifest.json options/* popup/*
 
 rm -rf dist/bibbot
 unzip dist/bibbot.zip -d dist/bibbot
