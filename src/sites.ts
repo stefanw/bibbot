a653b3e52947e3885ffb1ef3c71d65056a2bc181
Tibor Martini <tibor@users.noreply.github.com>
2023-05-01 18:42:38 +0200

    Updates
    * added source for stern.de
    * optimized query for capital.de
    * added geo.de

--- src/sites.ts
+++ src/sites.ts
@@ -1099,7 +1099,7 @@ const sites: Sites = {
     },
     source: 'genios.de',
     sourceParams: {
-      dbShortcut: 'STER'
+      dbShortcut: 'STER,STGL'
     }
   },
   'www.mittelbayerische.de': {
@@ -1150,7 +1150,7 @@ const sites: Sites = {
       headline: '.title__headline',
       date: 'time',
       main: '.article__body',
-      paywall: '.paid-barrier'
+      paywall: 'html:not(.has-paid-access) .title__logo--capital_plus'
     },
     dateRange: [10, 1],
     paragraphStyle: {
@@ -1159,6 +1159,23 @@ const sites: Sites = {
     source: 'genios.de',
     sourceParams: {
       dbShortcut: 'CAPI'
+    }
+  },
+  'www.geo.de': {
+    selectors: {
+      query: makeQueryFunc('.article__body p.text-element'),
+      headline: '.title__headline',
+      date: 'time',
+      main: '.article__body',
+      paywall: 'html:not(.has-paid-access) .title__logo--geo_plus'
+    },
+    dateRange: [10, 1],
+    paragraphStyle: {
+      selector: 'p.text-element'
+    },
+    source: 'genios.de',
+    sourceParams: {
+      dbShortcut: 'GEO,GEOS,GEOW,GESP,GEOE'
     }
   },
   'www.iz.de': {
