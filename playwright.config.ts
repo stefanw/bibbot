844f54a186c6cfa14bd0cf4e91c8ebae1b28eacb
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2024-01-26 00:24:25 +0100

    Disable firefox in tests
    - playwright does not support firefox extension testing currently
    - site tests don't need to run in multiple browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,17 +35,17 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
     }
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] }
+    // },
 
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] }
-    // }
+    // },
 
     /* Test against mobile viewports. */
     // {
